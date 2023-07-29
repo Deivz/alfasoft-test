60c064aeaa7327d6bb5f5c42bf8a038bfe4e1ce4
Deivz <carlosd_oliveira@hotmail.com>
2023-07-29 18:18:58 -0300

    Funcionalities being implemented: get contacts from firebase, add contact to firebase, add photo to firebase storage

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,24 +1,27 @@
 import React, { useEffect } from 'react'
 import styles from './Contacts.module.css'
 import Card from '../../components/Card/Card'
-import photo from '../../assets/muie.jpg'
+import photo from '../../assets/photo.jpg'
 
 export default function Contacts() {
 
-  const { contactsList, getContactsList } = useContext(ContactsContext);
+  // const { contactsList, getContactsList } = useContext(ContactsContext);
 
-  useEffect(() => {
-    getContactsList();
-  }, []);
+  // useEffect(() => {
+  //   getContactsList();
+  // }, []);
 
   return (
     <div className='container'>
       <div className={styles.contacts}>
-        {
+        {/* {
           contactsList.map((contact) => {
             return <Card key={contact.name} name={contact.name} email={contact.email} contact={contact.contact} picture={photo} />
           })
-        }
+        } */}
+        <Card name='Carlos Davi' email='carlos@email.com' contact='99999-9999' picture={photo} />
+        <Card name='Carlos Davi' email='carlos@email.com' contact='99999-9999' picture={photo} />
+        <Card name='Carlos Davi' email='carlos@email.com' contact='99999-9999' picture={photo} />
       </div>
     </div>
   )
